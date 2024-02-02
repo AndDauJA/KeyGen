794c26d647ca605a0f9af37a74494fe2fe44595e
AndDauJA <daujotasand@gmail.com>
2024-02-02 11:13:09 +0200

    Key Generator v3.0.5 removed, ErrorViewResolver modifiable map, taip pat sustabdziau security

--- src/main/java/lt/daujotas/utils/CustomErrorViewResolver.java
+++ src/main/java/lt/daujotas/utils/CustomErrorViewResolver.java
@@ -1,30 +1,30 @@
-package lt.daujotas.utils;
-
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Component
-public class CustomErrorViewResolver implements ErrorViewResolver {
-
-
-    // nuo nulio perrasem error
-
-    @Override
-    public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
-
-        if (status == HttpStatus.NOT_FOUND) {
-            return new ModelAndView("errors/404", model);
-
-        }
-       final Map<String, Object> myModelMap = new HashMap<>();
-        myModelMap.putAll(model);
-        myModelMap.put("myCustomAttribute", "Any text for attribute");
-        return new ModelAndView("customErrorPage", myModelMap);
-    }
-}
+//package lt.daujotas.utils;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Component
+//public class CustomErrorViewResolver implements ErrorViewResolver {
+//
+//
+//    // nuo nulio perrasem error
+//
+//    @Override
+//    public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+//
+//        if (status == HttpStatus.NOT_FOUND) {
+//            return new ModelAndView("errors/404", model);
+//
+//        }
+//       final Map<String, Object> myModelMap = new HashMap<>();
+//        myModelMap.putAll(model);
+//        myModelMap.put("myCustomAttribute", "Any text for attribute");
+//        return new ModelAndView("customErrorPage", myModelMap);
+//    }
+//}
