be1caf25e83c2d37d076da36c921e6416669c067
AndDauJA <daujotasand@gmail.com>
2024-02-02 12:22:03 +0200

    Key Generator v3.1.0 security login veikia

--- src/main/java/lt/daujotas/config/ApplicationConfig.java
+++ src/main/java/lt/daujotas/config/ApplicationConfig.java
@@ -2,10 +2,12 @@ package lt.daujotas.config;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
+@Configuration
 public class ApplicationConfig implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver() {
@@ -51,9 +54,15 @@ public class ApplicationConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
     }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
-        registry.addViewController("brigama/login").setViewName("brigama/login");
+        registry.addViewController("/login").setViewName("/brigama/login");
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("dbview/**")
+                .addResourceLocations("classpath;/static/dbview/");
     }
 
 }
--- src/main/java/lt/daujotas/security/config/SecurityConfig.java
+++ src/main/java/lt/daujotas/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package lt.daujotas.security.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,34 +14,34 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers ("/login/**",
-//                                "/",
-//                                "/client/**",
-//                                "/cart/**",
-//                                "/user/**",
-//                                "clientaccountform/**",
-//                                "/static/**",
-//                                "/js/**"
-//                                )
-//                        .permitAll()
-//                        .anyRequest()
-//                        .authenticated()
-//                )
-//                .formLogin(loginConfig -> loginConfig
-//                        .permitAll()
-//                        .loginPage("/login")
-//                        .loginProcessingUrl("/login")
-//                        .defaultSuccessUrl("/clientaccountform", true)
-//                        .usernameParameter("username")
-//                        .passwordParameter("password")
-//                )
-////                .logout(LogoutConfigurer::permitAll)
-//                .build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/",
+                                "/client/**",
+                                "/cart/**",
+                                "/user/**",
+                                "/clientaccountform/**"
+
+                        )
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+//                .formLogin(Customizer.withDefaults())
+                .formLogin(loginConfig -> loginConfig
+                        .permitAll()
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/clientaccountform", true)
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                )
+//                .logout(LogoutConfigurer::permitAll)
+                .build();
+    }
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
